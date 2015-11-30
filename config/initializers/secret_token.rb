286e42aa8fe2438bbd8a12a87ab0a38c05f50f5c
mugisha <mugishaj@outlook.com>
2015-11-30 18:45:41 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tachyon::Application.config.secret_key_base = '711a7e83ee8bdaca89c95665ad68e2e710a2b3f78251f9e361776dfdf9dc0cf7da6d0d347649bf8e43d9d4997f8d67adf38146c29639b329d1e73006737877e9'
