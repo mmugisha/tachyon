821000b4228df1ea38a13c7cb1ce92245a87e765
Mugisha <mugishaj@outlook.com>
2015-12-01 09:08:44 +0000

    add the about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Tachyon::Application.routes.draw do
   root 'static#index'
+  get '/about' => 'static#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
